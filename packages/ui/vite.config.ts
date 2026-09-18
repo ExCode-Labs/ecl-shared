b4a8b2bed712c81337e2ba2ac12e6676c819c3b3
Rahul Kumar <rahul.kumar@excodelabs.com>
2026-09-18 22:54:58 +0530

    fix(ui): inject library css automatically

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,10 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [libInjectCss(), react(), tailwindcss()],
   build: {
     lib: {
       entry: "src/index.ts",
@@ -14,7 +15,7 @@ export default defineConfig({
     },
 
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
     },
   },
 });
